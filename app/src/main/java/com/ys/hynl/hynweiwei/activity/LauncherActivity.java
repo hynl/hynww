79a1c8053f6bc696ea0a7dad337f32d822e36d13
hynl <hynl>
2016-11-07 20:58:57 +0800

    new project

---
+++ app/src/main/java/com/ys/hynl/hynweiwei/activity/LauncherActivity.java
@@ -0,0 +1,14 @@
+package com.ys.hynl.hynweiwei.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.ys.hynl.hynweiwei.R;
+
+public class LauncherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
---
+++ app/src/main/java/com/ys/hynl/hynweiwei/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ys.hynl.hynweiwei.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.ys.hynl.hynweiwei.R;
+
+/**
+ * Created by 何以奈良 on 2016/11/6.
+ */
+public class MainActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
