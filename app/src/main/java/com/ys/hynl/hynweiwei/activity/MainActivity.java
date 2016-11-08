ac6f6339fa4b2914dc540e314ee1d6c0a3351efb
hynl <hynliang@gmail.com>
2016-11-08 20:06:06 +0800

    2016.11.8|null

--- app/src/main/java/com/ys/hynl/hynweiwei/activity/MainActivity.java
+++ app/src/main/java/com/ys/hynl/hynweiwei/activity/MainActivity.java
@@ -1,17 +1,37 @@
 package com.ys.hynl.hynweiwei.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.FragmentActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.ys.hynl.hynweiwei.R;
 
 /**
  * Created by 何以奈良 on 2016/11/6.
  */
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends Activity {
+
+    FrameLayout fl;
+    CoordinatorLayout cl;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        fl = (FrameLayout) findViewById(R.id.fl_content);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View v =inflater.inflate(R.layout.activity_content,fl);
+        fl.addView(v);
+        cl = (CoordinatorLayout) fl.findViewById(R.id.cl_main_content);
     }
 }
