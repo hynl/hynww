21e01cdcb06f2e60b2638e1ee105caacac1d4264
hynl <hynliang@gmail.com>
2016-11-14 20:11:15 +0800

    20161114|起草学习使用

--- app/src/main/java/com/ys/hynl/hynweiwei/activity/MainActivity.java
+++ app/src/main/java/com/ys/hynl/hynweiwei/activity/MainActivity.java
@@ -2,22 +2,35 @@ package com.ys.hynl.hynweiwei.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 
 import com.ys.hynl.hynweiwei.R;
 
+import static com.ys.hynl.hynweiwei.R.string.toolbar_title;
+
 /**
  * Created by 何以奈良 on 2016/11/6.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+ ,View.OnClickListener{
 
     FrameLayout fl;
-    CoordinatorLayout cl;
-
+    NavigationView nv_left;
+    DrawerLayout dl_main;
+    Toolbar toolbar_main;
+    CollapsingToolbarLayout ctl_main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +43,31 @@ public class MainActivity extends Activity {
     private void initView() {
         fl = (FrameLayout) findViewById(R.id.fl_content);
         LayoutInflater inflater = LayoutInflater.from(this);
-        View v =inflater.inflate(R.layout.activity_content,fl);
-        fl.addView(v);
-        cl = (CoordinatorLayout) fl.findViewById(R.id.cl_main_content);
+        View v =inflater.inflate(R.layout.activity_content,null);
+        CoordinatorLayout cl = (CoordinatorLayout) v.findViewById(R.id.cl_main_content);
+        fl.addView(cl);
+
+        dl_main = (DrawerLayout) findViewById(R.id.dl_main);
+        nv_left = (NavigationView) findViewById(R.id.nav_left);
+        nv_left.setNavigationItemSelectedListener(this);
+
+        ctl_main = (CollapsingToolbarLayout) cl.findViewById(R.id.ctl_scale_toolbar);
+        toolbar_main = (Toolbar) cl.findViewById(R.id.toolbar_main);
+        setSupportActionBar(toolbar_main);
+        ctl_main.setTitle("测试");
+        toolbar_main.setNavigationOnClickListener(this);
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        item.setChecked(true);
+        dl_main.closeDrawers();
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
